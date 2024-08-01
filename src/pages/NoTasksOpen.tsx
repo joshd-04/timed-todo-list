545ba405203a3aa55e9d51418dc65dceb3bf446c
joshd-04 <joshdyal04@gmail.com>
2024-08-01 16:36:51 +0100

    add fullstop

--- src/pages/NoTasksOpen.tsx
+++ src/pages/NoTasksOpen.tsx
@@ -1,7 +1,7 @@
 export default function NoTasksOpen() {
   return (
     <h2 className="message">
-      No tasks open, create or click a task to view it
+      No tasks open, create or click a task to view it.
     </h2>
   );
 }
